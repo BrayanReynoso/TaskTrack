8dfc593887da879e245e963c4eb07ebe92514cfe
Brayan Reynoso Macedo <20213tn084@utez.edu.mx>
2024-09-01 11:41:30 -0600

    [ADD]
    Project creation

---
+++ api-tasktrack/src/main/java/com/mx/tasktrack/ApiTasktrackApplication.java
@@ -0,0 +1,13 @@
+package com.mx.tasktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTasktrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTasktrackApplication.class, args);
+    }
+
+}
